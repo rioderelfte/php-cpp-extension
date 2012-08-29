137d497543bcb417caeb6d15f7cf0aca422836ef
Florian Sowade <f.sowade@r9e.de>
2012-08-29 02:43:11 +0200

    use the system wide php in php.sh

--- php.sh
+++ php.sh
@@ -3,4 +3,4 @@
 file="$(readlink -e "${0}")"
 dir="$(dirname "${file}")"
 
-"${dir}/../../php-install2/bin/php" -d extension="${dir}/modules/cpptest.so" "${@}"
+php -d extension="${dir}/modules/cpptest.so" "${@}"
